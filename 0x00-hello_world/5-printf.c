a1c3c19c2cdeda5a5beb69bb407b13fcd933a6a2
SEA471 <seallen422@mail.com>
2021-09-28 01:16:26 +0000

    Add c program for printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * function_name - printf
+ *
+ * Return: 0 = Success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the oucome is a piece of art,\n");
+	return (0);
+}
