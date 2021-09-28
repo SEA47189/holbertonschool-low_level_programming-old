48ff4c0890e013807c9b2909cbb9933f3e85c584
SEA471 <seallen422@mail.com>
2021-09-28 20:50:48 +0000

    Add file to print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Function_name - printf
+ *
+ * Return: 0 = Success
+ */
+int main(void)
+{
+	int n;
+	/* Loop to print single digits of base 10 */
+	for (n = 0; n <= 9; n++)
+	{
+		/* Print numbers */
+		printf("%d", n);
+	}
+	{
+		/* Print a new line */
+		printf("\n");
+	}
+	return (0);
+}
