f27be26395a100a93f03b4ea9dbbcc35234b1446
SEA471 <seallen422@mail.com>
2021-09-30 01:51:25 +0000

    Add file to find the absolute value of a given number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs - computes absolute value
+ *
+ *@n: number input
+ *
+ *Return: 0
+ */
+int _abs(int n)
+{
+	/* Find the absolute value of given number */
+	if (n > 0)
+		n = n;
+	if (n < 0)
+		n = (n * -1);
+	if (n == 0)
+		n = n;
+	return (n);
+}
