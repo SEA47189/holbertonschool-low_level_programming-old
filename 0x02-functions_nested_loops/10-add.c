853fe5b7881ca4faa07a1bf10db4d9044ef1b867
SEA471 <seallen422@mail.com>
2021-09-30 04:37:09 +0000

    Add file to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *add - add two numbers
+ *
+ *@int: numbers
+ *
+ *Return: 0 = Success
+ */
+int add(int, int)
+{
+	int a;
+	int b;
+	int sum;
+
+	sum = a + b;
+	{
+		_putchar(sum + '0');
+		_putchar('\n');
+	}
+	return (0);
+}
