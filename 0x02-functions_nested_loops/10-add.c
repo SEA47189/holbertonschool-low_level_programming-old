08d7b6b499def5b847a3de4d7b2d63a92208badc
SEA471 <seallen422@mail.com>
2021-10-01 01:54:45 +0000

    Corrected file to add two numbers together

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,9 @@
 /**
  *add - add two numbers
  *
- *@a: numbers
+ *@a: 1st number
  *
- *@b: number
+ *@b: 2nd number
  *
  *Return: 0 = Success
  */
@@ -13,22 +13,5 @@ int add(int a, int b)
 	int c;
 
 	c = a + b;
-	if (c > 0)
-	{
-		c = (c % 10);
-		_putchar(c + '0');
-	}
-	if (c < 0)
-	{
-		c = (c * -1);
-		c = (c % 10);
-		_putchar(c + '0');
-	}
-	if (c == 0)
-	{
-		c = (c % 10);
-		_putchar(c + '0');
-	}
-	_putchar('\n');
-	return (0);
+	return (c);
 }
