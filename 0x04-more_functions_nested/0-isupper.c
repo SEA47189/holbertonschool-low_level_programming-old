a9f563603978a35c92b1d5fa879b3f2efda52c93
SEA471 <seallen422@mail.com>
2021-10-05 04:14:56 +0000

    Add file and program to find uppercase letters

---
+++ 0x04-more_functions_nested/0-isupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * Function_name - putchar
+ *
+ * putchar - prints letters
+ *
+ *@c: character to print
+ *
+ * Return: 0 = Success
+ */
+int _isupper(int c)
+
+{
+	/* Loop to print upper case alphabet */
+	if ((c > 65) && (c < 90))
+	{
+		putchar(c);
+		putchar(':');
+		putchar(' ');
+		return (1);
+	}
+	if ((c > 97) && (c < 122))
+	{
+		putchar(c);
+		putchar(':');
+		putchar(' ');
+	}
+	return(0);
+}
