89745873734803fe276a4498c10426ade80e8b96
SEA471 <seallen422@mail.com>
2021-10-07 23:08:46 +0000

    Add file to print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,20 +14,12 @@ void print_rev(char *s)
 	int a;
 
 	for (a = 0; s[a] != '\0'; a++)
+		;
+	for (a--; a >= 0; a--)
 	{
 		_putchar(s[a]);
 	}
 	{
 		_putchar('\n');
-	}
-	if ('\n')
-	{
-		for (a--; a >= 0; a--)
-		{
-			_putchar(s[a]);
-		}
-		{
-			_putchar('\n');
-		}
 	}
 }
