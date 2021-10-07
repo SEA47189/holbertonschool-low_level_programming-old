984d630a38d1b8ff2e6521a6f2882918394094ce
SEA471 <seallen422@mail.com>
2021-10-07 05:09:52 +0000

    Add file to reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <strings.h>
+/**
+ *print_rev - prints string in reverse
+ *
+ *@s: - string given
+ *
+ *function_name - putchar
+ *
+ *Return: void
+ */
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		_putchar(s[a]);
+	}
+	{
+		_putchar('\n');
+	}
+	if ('\n')
+	{
+		for (a--; a >= 0; a--)
+		{
+			_putchar(s[a]);
+		}
+		{
+			_putchar('\n');
+		}
+	}
+}
