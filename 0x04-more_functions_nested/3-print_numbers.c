f101001308ddd34bdd46914c4ed9808afc8b44c0
SEA471 <seallen422@mail.com>
2021-10-08 04:41:11 +0000

    Function to print numbers

---
+++ 0x04-more_functions_nested/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include "main.h"
+/**
+ *print_numbers - given numbers
+ *
+ *function_name - putchar - prints
+ *
+ *Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	{
+		_putchar('\n');
+	}
+}
