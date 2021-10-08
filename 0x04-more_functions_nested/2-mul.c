391a10b0255415c2b7e24a286e74dc2335f650be
SEA471 <seallen422@mail.com>
2021-10-08 04:30:00 +0000

    Add function to multiply two digits

---
+++ 0x04-more_functions_nested/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include "main.h"
+/**
+ *mul - multiplies numbers
+ *
+ *@a: first number
+ *
+ *@b: second number
+ *
+ *Return: 0 = Success
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
