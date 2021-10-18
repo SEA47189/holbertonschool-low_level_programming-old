a04ff57fa669a2b2e534b9d66d66491b406aca4b
SEA47189 <seallen422@gmail.com>
2021-10-18 04:47:24 +0000

    Add file and function prototype

---
+++ 0x0A-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include "main.h"
+/**
+ *_strcpy - copies string
+ *
+ *@d - string destination
+ *@s - string source
+ *
+ *Return: void
+ */
+char *_strcpy(char *dest, char *src)
